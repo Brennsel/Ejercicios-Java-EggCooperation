45927103093a27816607a64c42d98d29baab7235
Brennsel <brennsell@gmail.com>
2023-04-07 19:42:23 -0300

    See añadio ej 6 y se modifico ej4 y ej5

---
+++ src/Arreglos/Ej6.java
@@ -0,0 +1,13 @@
+package Arreglos;
+ /*
+ Un cuadrado mágico 3 x 3 es una matriz 3 x 3 formada por números del 1 al 9 donde la suma de sus filas,
+ sus columnas y sus diagonales son idénticas. Crear un programa que permita introducir un cuadrado por teclado
+ y determine si este cuadrado es mágico o no. El programa deberá comprobar que los números introducidos son correctos,
+ es decir, están entre el 1 y el 9.
+  */
+public class Ej6 {
+
+     public static void main(String[] args) {
+
+     }
+}
